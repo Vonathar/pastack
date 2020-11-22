ed1594ade7130f046f6990ec52733dd0a7a61011
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-11-22 15:29:55 +0000

    Add tests for AnswerBuilder.

---
+++ src/test/java/uk/caputo/pastack/AnswerBuilderTest.java
@@ -0,0 +1,40 @@
+package uk.caputo.pastack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/** A class used to test the AnswerBuilder by building an Answer object. */
+@SpringBootTest
+class AnswerBuilderTest {
+
+  @Autowired AnswerBuilder answerBuilder;
+
+  @Test
+  public void build_BuilderFieldsPopulated_ShouldReturnBuildValidAnswerObject()
+      throws MalformedURLException {
+    URL mockUrl = new URL("https://www.stackoverflow.com");
+    Date mockDate = new Date();
+    Answer answer =
+        answerBuilder
+            .setUpvotes(100)
+            .setBestAnswer(true)
+            .setUrl(mockUrl)
+            .setDate(mockDate)
+            .setAuthor("Jon Doe")
+            .setContent("Lorem ipsum.")
+            .build();
+
+    assertThat(answer.isBestAnswer()).isTrue();
+    assertThat(answer.getUpvotes()).isEqualTo(100);
+    assertThat(answer.getUrl()).isEqualTo(mockUrl);
+    assertThat(answer.getDate()).isEqualTo(mockDate);
+    assertThat(answer.getAuthor()).isEqualTo("Jon Doe");
+    assertThat(answer.getContent()).isEqualTo("Lorem ipsum.");
+  }
+}
