1f62c7008a18deffd442642aadc6d9d454961827
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-11-22 20:37:22 +0000

    Implement Answer, AnswerBuilder classes.
    As multiple instances of Answer have to be dynamically created, the creation implements the builder pattern. This will also facilitate the addition of new features at a later date.

---
+++ src/main/java/uk/caputo/pastack/Answer.java
@@ -0,0 +1,65 @@
+package uk.caputo.pastack;
+
+import java.net.URL;
+import java.util.Date;
+
+/**
+ * A class that represents a single answer to a question on the StackOverflow website. It is not
+ * managed by Spring, and relies on AnswerBuilder for instantiation. The only exposed methods are
+ * getters, in order to preserve immutability.
+ */
+public class Answer {
+
+  private final int upvotes;
+  private final String question;
+  private final boolean isBestAnswer;
+  private final URL url;
+  private final Date date;
+  private final String author;
+  private final String content;
+
+  public Answer(
+      int upvotes,
+      String question,
+      boolean isBestAnswer,
+      URL url,
+      Date date,
+      String author,
+      String content) {
+    this.upvotes = upvotes;
+    this.question = question;
+    this.isBestAnswer = isBestAnswer;
+    this.url = url;
+    this.date = date;
+    this.author = author;
+    this.content = content;
+  }
+
+  public int getUpvotes() {
+    return upvotes;
+  }
+
+  public boolean isBestAnswer() {
+    return isBestAnswer;
+  }
+
+  public URL getUrl() {
+    return url;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public String getQuestion() {
+    return question;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
